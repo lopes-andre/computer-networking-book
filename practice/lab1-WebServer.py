94e704e128b384afea319757057174649e994c29
lopes-andre <lopes.andre@outlook.com>
2021-05-29 13:33:32 -0300

    Update socket port

--- practice/lab1-WebServer.py
+++ practice/lab1-WebServer.py
@@ -8,49 +8,51 @@ over the TCP connection to the requesting browser. If a browser requests a file 
 not present in your server, your server should return a “404 Not Found” error message. '''
 
 # Import sys and socket module
-from socket import *    
+from socket import *
 import sys
 
 # Create a TCP server socket: AF_INET for IPv4 and SOCK_STREAM for TCP
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Prepare a server socket
-serverPort = 3085 # Port number
+serverPort = 80  # Port number
 serverSocket.bind(("", serverPort))
-serverSocket.listen(1) # Listening to 1 conection
+serverSocket.listen(1)  # Listening to 1 conection
 
 while True:
-	print("Ready to serve...") # Establish the connection
-	
-	connectionSocket, addr = serverSocket.accept()
-	
-	# If an exception occurs during the execution of try clause
-	# the rest of the clause is skipped
-	# If the exception type matches the word after except
-	# the except clause is executed
-	try:
-		message =  connectionSocket.recv(1024)
-		filename = message.split()[1] # Gets the second part of the HTTP header
-		f = open(filename[1:]) # Takes off the '\' from the path name
-		outputdata = f.read() # Puts file in a buffer
+    print("Ready to serve...")  # Establish the connection
 
-		# Send one HTTP header line into socket
-		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
- 
-		# Send the content of the requested file to the client
-		for i in range(0, len(outputdata)):  
-			connectionSocket.send(outputdata[i])
-		connectionSocket.send("\r\n")
-		
-		connectionSocket.close() # Close the connection socket
+    connectionSocket, addr = serverSocket.accept()
 
-	except IOError:
-		# Send response message for file not found
-		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
-		connectionSocket.send("<html><head><title>404 Not Found</title></head><body><center><h1>404 Page Not Found</h1></center></body></html>\r\n")
-		
-		# Close client socket
-		connectionSocket.close()
+    # If an exception occurs during the execution of try clause
+    # the rest of the clause is skipped
+    # If the exception type matches the word after except
+    # the except clause is executed
+    try:
+        message = connectionSocket.recv(1024)
+        # Gets the second part of the HTTP header
+        filename = message.split()[1]
+        f = open(filename[1:])  # Takes off the '\' from the path name
+        outputdata = f.read()  # Puts file in a buffer
 
-serverSocket.close() # Close server
-sys.exit() # Terminate the program after sending the corresponding data
+        # Send one HTTP header line into socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+        connectionSocket.send("\r\n")
+
+        connectionSocket.close()  # Close the connection socket
+
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
+        connectionSocket.send(
+            "<html><head><title>404 Not Found</title></head><body><center><h1>404 Page Not Found</h1></center></body></html>\r\n")
+
+        # Close client socket
+        connectionSocket.close()
+
+serverSocket.close()  # Close server
+sys.exit()  # Terminate the program after sending the corresponding data
